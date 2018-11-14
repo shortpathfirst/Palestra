81e2a4b32dda25e08eedc5fc87eaf35875bcfec9
cecy9 <cecy9@141.250.219.111>
2018-11-14 17:21:47 +0100

    h

--- Palestra/src/main/java/Main.java
+++ Palestra/src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		LocalDate myDate =LocalDate.parse("2014-02-14");
 //		String a =  GestorePalestra.inserisciUtente("Mario", "Rossi", "0792893", "aoshdqeqw213123","m","perugia", myDate,"studente");
-		System.out.println(myDate.getDayOfWeek());
+		System.out.println(myDate);
 
 		
 	}
